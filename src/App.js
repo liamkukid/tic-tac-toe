c09b75027f87b87fc3446c9904c00a983650482b
Kulik Dzmitry <liamkukid@gmail.com>
2023-04-28 16:03:04 +0200

    logo removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Marek_yoga from './Marek_yoga.jpg';
 import Marek_walk from './Marek_walk.jpg';
 import './App.css';
@@ -7,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Marek_yoga} className="Marek_yoga" alt="logo" />
-        <img src={Marek_walk} className="Marek_walk" alt="logo" />
+        <img src={Marek_yoga} className="Marek_yoga" />
+        <img src={Marek_walk} className="Marek_walk" />
         <p>
           Marek's blog.
         </p>
