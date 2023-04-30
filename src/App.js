648e7e47800f3634c06b093fd2126fae743a209f
Kulik Dzmitry <liamkukid@gmail.com>
2023-04-30 14:18:09 +0200

    function Game bug fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
