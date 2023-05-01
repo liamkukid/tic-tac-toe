42f0cce999a8e342a1ac31ff07f369d07eedee81
Kulik Dzmitry <liamkukid@gmail.com>
2023-05-01 10:06:53 +0200

    refactoring

---
+++ src/Square/Square.js
@@ -0,0 +1,5 @@
+import style from "./square.module.scss";
+
+export default function Square({ value, onSquareClick }) {
+    return <button className={style.square} onClick={onSquareClick}>{value}</button>
+  }
